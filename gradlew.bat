225ea2a2883193967a370573f058b73b6b8a8636
Ghh Hmg <296308030+ghhhmg11-coder@users.noreply.github.com>
2026-06-26 22:36:40 +0300

    Add gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,7 @@
+@rem Gradle startup script for Windows
+@if "%DEBUG%" == "" @echo off
+set DIRNAME=%~dp0
+set APP_BASE_NAME=%~n0
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set CLASSPATH=%DIRNAME%\gradle\wrapper\gradle-wrapper.jar
+java %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
